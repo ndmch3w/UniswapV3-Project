e20f80523cce193a72749ca2394275d02499ec22
vuquyen03 <quyenvungoc1711@gmail.com>
2024-06-02 05:54:56 +0700

    change Oracle

--- Uniswap-v3/contracts/UniswapV3Oracle.sol
+++ Uniswap-v3/contracts/UniswapV3Oracle.sol
@@ -22,22 +22,14 @@ contract UniswapV3Oracle {
         (int56[] memory tickCumulatives, ) = uniswapv3Pool.observe(secondAgos);
 
         int56 tickCumulativesDiff = tickCumulatives[1] - tickCumulatives[0];
-        uint32 period = _seconds; // period in seconds
+        uint56 period = _seconds; // period in seconds
 
-<<<<<<< HEAD
-        require(period > 0, "Period must be greater than 0");
-
-        int24 timeWeightedAverageTick = int24(tickCumulativesDiff / int56(int32(period)));
-        
-        require(timeWeightedAverageTick >= TickMath.MIN_TICK && timeWeightedAverageTick <= TickMath.MAX_TICK, "TWAT out of bounds");
-=======
         int56 timeWeightedAverageTick = tickCumulativesDiff / -int56(period);
->>>>>>> fe1a07660dcdc9b8ca6d7c7758f7012bda91f1b8
 
         uint256 decimalToken0 = IERC20Metadata(uniswapv3Pool.token0()).decimals();
         uint256 decimalToken1 = IERC20Metadata(uniswapv3Pool.token1()).decimals();
 
-        uint160 sqrtRatioX96 = TickMath.getSqrtRatioAtTick(timeWeightedAverageTick);
+        uint160 sqrtRatioX96 = TickMath.getSqrtRatioAtTick(int24(timeWeightedAverageTick));
         uint256 ratioX192 = uint256(sqrtRatioX96) * sqrtRatioX96;
 
         uint256 adjustedPrice = (ratioX192 * 1e18) >> (96 * 2); // Price in token1 units
